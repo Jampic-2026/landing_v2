3764ecc19a5e4d0113ee77e06cb6915fb491e911
Laura Nuñez <njm0031533@est.univalle.edu>
2026-08-06 16:10:39 -0400

    Update links.ts

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,5 +1,5 @@
 export const productLinks = {
-  webApp: import.meta.env.VITE_WEB_APP_URL ?? 'https://app.jampic.com',
+  webApp: import.meta.env.VITE_WEB_APP_URL ?? 'https://jampic.doc-ia.cloud',
   androidApk:
     import.meta.env.VITE_ANDROID_APK_URL ?? '/downloads/jampic_V1.0.0.apk',
 } as const;
